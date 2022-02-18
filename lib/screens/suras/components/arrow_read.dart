32ba43c24cd55a7c7bff1b4c2cb869be4492df92
gurkay <g_basyigit@hotmail.com>
2022-02-18 23:21:06 +0300

    first commit

--- lib/screens/suras/components/arrow_read.dart
+++ lib/screens/suras/components/arrow_read.dart
@@ -51,6 +51,17 @@ class _ArrowReadState extends State<ArrowRead> {
     42000
   ];
 
+  final Map<int, double> _versesDurationPositions = {
+    1: 6380,
+    2: 11066,
+    3: 15618,
+    4: 19924,
+    5: 27501,
+    6: 33057,
+    7: 52088,
+  };
+
+  int _versesLenght = 0;
   List<double> _bottomGreenArrow = [];
   List<double> _rightGreenArrow = [];
   Timer? _timer;
@@ -64,12 +75,12 @@ class _ArrowReadState extends State<ArrowRead> {
   }
 
   Future<void> _init() async {
-    _isGreenUpArrow =
-        List.generate(widget.modelVerses.length, (index) => false);
-    _selected = List.generate(widget.modelVerses.length, (index) => false);
-    _rightGreenArrow = List.generate(widget.modelVerses.length, (index) => 0.0);
-    _bottomGreenArrow =
-        List.generate(widget.modelVerses.length, (index) => 0.0);
+    _versesLenght = widget.modelVerses.length;
+
+    _isGreenUpArrow = List.generate(_versesLenght, (index) => false);
+    _selected = List.generate(_versesLenght, (index) => false);
+    _rightGreenArrow = List.generate(_versesLenght, (index) => 0.0);
+    _bottomGreenArrow = List.generate(_versesLenght, (index) => 0.0);
     _scrollController.addListener(() {
       _scrollListener();
     });
@@ -97,14 +108,13 @@ class _ArrowReadState extends State<ArrowRead> {
 
   void getArrowUp() {
     if (widget.onChangeEnd != null) {
-      print('onChanged:::${widget.onChanged}');
       print('onChangeEnd:::${widget.onChangeEnd}');
     } else {
       print('start');
     }
 
     // print('duration: ${widget.duration.inMilliseconds.toDouble()}');
-    // print('position: ${widget.position.inMilliseconds.toDouble()}');
+    print('position: ${widget.position.inMilliseconds.toDouble()}');
     // print('bufferedPosition: ${widget.bufferedPosition.inSeconds.toDouble()}');
 
     // print('rightGreenArrow: ${_rightGreenArrow}');
@@ -112,115 +122,115 @@ class _ArrowReadState extends State<ArrowRead> {
 
     Size size = MediaQuery.of(context).size;
 
-    if (widget.position.inMilliseconds.toDouble() != 0) {
-      if (widget.position.inMilliseconds.toDouble() < 6380) {
-        setState(() {
-          _selected[0] = true;
-          _isGreenUpArrow[0] = true;
-          _rightGreenArrow[0] += 2.0;
-        });
-      } else if (widget.position.inMilliseconds.toDouble() < 12380) {
-        setState(() {
-          _isGreenUpArrow[0] = false;
-          _selected[0] = false;
-          _bottomGreenArrow[0] = 0;
-          _rightGreenArrow[0] = 0;
-        });
+    if (widget.position.inMilliseconds.toDouble() < 6380) {
+      setState(() {
+        _selected[0] = true;
+        _isGreenUpArrow[0] = true;
+        _rightGreenArrow[0] += 2.0;
+      });
+    } else if (widget.position.inMilliseconds.toDouble() < 12380) {
+      setState(() {
+        _isGreenUpArrow[0] = false;
+        _selected[0] = false;
+        _bottomGreenArrow[0] = 0;
+        _rightGreenArrow[0] = 0;
+      });
 
-        setState(() {
-          _selected[1] = true;
-          _isGreenUpArrow[1] = true;
-          _rightGreenArrow[1] += 2.0;
-          print('1 size:::${(size.height * 0.050)}');
-          _scrollJumpTo(size.height * 0.050);
-        });
-      } else if (widget.position.inMilliseconds.toDouble() < 18380) {
-        setState(() {
-          _isGreenUpArrow[1] = false;
-          _selected[1] = false;
-          _bottomGreenArrow[1] = 0;
-          _rightGreenArrow[1] = 0;
-        });
+      setState(() {
+        _selected[1] = true;
+        _isGreenUpArrow[1] = true;
+        _rightGreenArrow[1] += 2.0;
+        print('1 size:::${(size.height * 0.050)}');
+        _scrollJumpTo(size.height * 0.050);
+      });
+    } else if (widget.position.inMilliseconds.toDouble() < 18380) {
+      setState(() {
+        _isGreenUpArrow[1] = false;
+        _selected[1] = false;
+        _bottomGreenArrow[1] = 0;
+        _rightGreenArrow[1] = 0;
+      });
 
-        setState(() {
-          _selected[2] = true;
-          _isGreenUpArrow[2] = true;
-          _rightGreenArrow[2] += 2.0;
-          print('2 size:::${(size.height * 0.10)}');
-          _scrollJumpTo(size.height * 0.10);
-        });
-      } else if (widget.position.inMilliseconds.toDouble() < 24380) {
-        setState(() {
-          _isGreenUpArrow[2] = false;
-          _selected[2] = false;
-          _bottomGreenArrow[2] = 0;
-          _rightGreenArrow[2] = 0;
-        });
+      setState(() {
+        _selected[2] = true;
+        _isGreenUpArrow[2] = true;
+        _rightGreenArrow[2] += 2.0;
+        print('2 size:::${(size.height * 0.10)}');
+        _scrollJumpTo(size.height * 0.10);
+      });
+    } else if (widget.position.inMilliseconds.toDouble() < 24380) {
+      setState(() {
+        _isGreenUpArrow[2] = false;
+        _selected[2] = false;
+        _bottomGreenArrow[2] = 0;
+        _rightGreenArrow[2] = 0;
+      });
 
-        setState(() {
-          _selected[3] = true;
-          _isGreenUpArrow[3] = true;
-          _rightGreenArrow[3] += 2.0;
-          print('3 size:::${(size.height * 0.450)}');
-          _scrollJumpTo(size.height * 0.450);
-        });
-      } else if (widget.position.inMilliseconds.toDouble() < 30380) {
-        setState(() {
-          _isGreenUpArrow[3] = false;
-          _selected[3] = false;
-          _bottomGreenArrow[3] = 0;
-          _rightGreenArrow[3] = 0;
-        });
+      setState(() {
+        _selected[3] = true;
+        _isGreenUpArrow[3] = true;
+        _rightGreenArrow[3] += 2.0;
+        print('3 size:::${(size.height * 0.450)}');
+        _scrollJumpTo(size.height * 0.450);
+      });
+    } else if (widget.position.inMilliseconds.toDouble() < 30380) {
+      setState(() {
+        _isGreenUpArrow[3] = false;
+        _selected[3] = false;
+        _bottomGreenArrow[3] = 0;
+        _rightGreenArrow[3] = 0;
+      });
 
-        setState(() {
-          _selected[4] = true;
-          _isGreenUpArrow[4] = true;
-          _rightGreenArrow[4] += 2.0;
-          print('4 size:::${(size.height * 0.650)}');
-          _scrollJumpTo(size.height * 0.650);
-        });
-      } else if (widget.position.inMilliseconds.toDouble() < 36380) {
-        setState(() {
-          _isGreenUpArrow[4] = false;
-          _selected[4] = false;
-          _bottomGreenArrow[4] = 0;
-          _rightGreenArrow[4] = 0;
-        });
+      setState(() {
+        _selected[4] = true;
+        _isGreenUpArrow[4] = true;
+        _rightGreenArrow[4] += 2.0;
+        print('4 size:::${(size.height * 0.650)}');
+        _scrollJumpTo(size.height * 0.650);
+      });
+    } else if (widget.position.inMilliseconds.toDouble() < 36380) {
+      setState(() {
+        _isGreenUpArrow[4] = false;
+        _selected[4] = false;
+        _bottomGreenArrow[4] = 0;
+        _rightGreenArrow[4] = 0;
+      });
 
-        setState(() {
-          _selected[5] = true;
-          _isGreenUpArrow[5] = true;
-          _rightGreenArrow[5] += 2.0;
-        });
-        _scrollJumpTo(_scrollController.position.maxScrollExtent);
-      } else if (widget.position.inMilliseconds.toDouble() < 42380) {
-        setState(() {
-          _isGreenUpArrow[5] = false;
-          _selected[5] = false;
-          _bottomGreenArrow[5] = 0;
-          _rightGreenArrow[5] = 0;
-        });
+      setState(() {
+        _selected[5] = true;
+        _isGreenUpArrow[5] = true;
+        _rightGreenArrow[5] += 2.0;
+      });
+      _scrollJumpTo(_scrollController.position.maxScrollExtent);
+    } else if (widget.position.inMilliseconds.toDouble() < 42380) {
+      setState(() {
+        _isGreenUpArrow[5] = false;
+        _selected[5] = false;
+        _bottomGreenArrow[5] = 0;
+        _rightGreenArrow[5] = 0;
+      });
 
-        setState(() {
-          _selected[6] = true;
-          _isGreenUpArrow[6] = true;
-          _rightGreenArrow[6] += 2.0;
-        });
-      }
+      setState(() {
+        _selected[6] = true;
+        _isGreenUpArrow[6] = true;
+        _rightGreenArrow[6] += 2.0;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     if (widget.onChangeEnd != null) {
-      getArrowUp();
+      if (widget.position.inMilliseconds.toDouble() != 0) {
+        getArrowUp();
+      }
     }
     Size size = MediaQuery.of(context).size;
     return Container(
       height: size.height * 0.80,
       child: ListView.builder(
         controller: _scrollController,
-        itemCount: widget.modelVerses.length,
+        itemCount: _versesLenght,
         itemBuilder: (ctx, index) {
           return Card(
             elevation: 7,
